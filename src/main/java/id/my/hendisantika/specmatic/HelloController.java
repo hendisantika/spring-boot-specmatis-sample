54f6790fc8b4a35f52069fbe9c07ecf40270420e
hendisantika <hendisantika@yahoo.co.id>
2025-07-17 10:34:32 +0700

    feat: Update HelloController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/specmatic/HelloController.java
+++ src/main/java/id/my/hendisantika/specmatic/HelloController.java
@@ -1,5 +1,11 @@
 package id.my.hendisantika.specmatic;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +23,15 @@ import java.util.Map;
  * To change this template use File | Settings | File Templates.
  */
 @RestController
+@Tag(name = "Hello", description = "Hello World API")
 public class HelloController {
 
+    @Operation(summary = "Get a hello message", description = "Returns a simple hello world message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the hello message",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Map.class)))
+    })
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of("message", "Hello, World!");
